a7d7632d10d072a5ddb7dfcf3c248022504a7ca2
VitorReisDev <hugov5323@gmail.com>
2022-11-20 11:00:10 -0300

    Verificando o cadastro do usuario e validando senha

--- codigo/cadastro/js/dados.js
+++ codigo/cadastro/js/dados.js
@@ -72,6 +72,7 @@ window.onload = function () {
       validConfirmSenha = true;
     }
   });
+
   codigo.addEventListener("keyup", () => {
     if (codigo.value.length <= 3) {
       codigo.setAttribute("style", "border-color: red");
@@ -91,7 +92,7 @@ window.onload = function () {
       validOp = true;
     }
   });
-
+  
   cadastro1.onclick = function cadastrar() {
     if (
       validNome &&
@@ -100,23 +101,41 @@ window.onload = function () {
       validSenha &&
       validConfirmSenha &&
       validCodigo
-    ) {
-      let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
-
-      listaUser.push({
-        nomeCad: nome.value,
-        sobrenomeCad: sobrenome.value,
-        emailCad: email.value,
-        senhaCad: senha.value,
-        codigoCad: codigo.value,
+      ) {
+        let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
+        
+        listaUser.push({
+          nomeCad: nome.value,
+          sobrenomeCad: sobrenome.value,
+          codigoCad: codigo.value,
         opcaoCad: op.value
       });
 
+      let emailUser = JSON.parse(localStorage.getItem("emailUser") || "[]");
+      
+      emailUser.push(
+        email.value
+        );
+        
+        let senhaUser = JSON.parse(localStorage.getItem("senhaUser") || "[]");
+        senhaUser.push(
+          senha.value
+        );
+        
+        if(senha.value != confirmaSenha.value){
+          alert("Senha incorreta")
+          
+        }else{
+        localStorage.setItem("listaUser", JSON.stringify(listaUser));
+        localStorage.setItem("emailUser", JSON.stringify(emailUser));
+        localStorage.setItem("senhaUser", JSON.stringify(senhaUser));
+      }
+      
       foto.addEventListener("change", () => {
         const fr = new FileReader();
-
+        
         fr.readAsDataURL(foto.files[0]);
-
+        
         fr.addEventListener("load", () => {
           const url = fr.result;
 
@@ -124,22 +143,20 @@ window.onload = function () {
         });
       });
 
-      localStorage.setItem("listaUser", JSON.stringify(listaUser));
-
       msgSuccess.setAttribute("style", "display: block");
       msgSuccess.innerHTML = "<strong>Cadastrado com sucesso!</strong>";
       msgError.setAttribute("style", "display: none");
       msgError.innerHTML = "";
-
+      
       setTimeout(() => {
         window.location.href =
-          "http://127.0.0.1:5500/tiaw2022-manha-carona-universitaria/codigo/login/index.html";
+        "http://127.0.0.1:5500/tiaw2022-manha-carona-universitaria/codigo/login/index.html";
       }, 3000);
-
+      
     } else {
       msgError.setAttribute("style", "display: block");
       msgError.innerHTML =
-        "<strong>Preencha todos os campos corretamente antes de cadastrar!</strong>";
+      "<strong>Preencha todos os campos corretamente antes de cadastrar!</strong>";
       msgSuccess.innerHTML = "";
       msgSuccess.setAttribute("style", "display: none");
     }
---
+++ codigo/login/script.js
@@ -0,0 +1,18 @@
+// Função para verificar o cadastro do usuario
+function logar(){
+    var email = document.getElementById("inputEmail");
+    var senha = document.getElementById("inputPassword");
+    var cadEmail = localStorage.getItem("emailUser");
+    var cadSenha = localStorage.getItem("senhaUser")
+
+    const parsedEmail = cadEmail.slice(2, cadEmail.length -2)
+    const parsedSenha = cadSenha.slice(2, cadSenha.length -2)
+
+    if(email.value === parsedEmail && senha.value === parsedSenha){
+        alert("Usuario autenticado!")
+        window.location.href = "/tiaw2022-manha-carona-universitaria/codigo/tela principal/index.html"
+    }else {
+        alert("Email ou senha invalidos!");
+    }
+   
+}
